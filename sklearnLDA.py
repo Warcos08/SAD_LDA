6e453f47029d3655903e5222c77f5c15944a423f
Warcos08 <mmerino08p@gmail.com>
2022-05-27 17:45:40 +0200

    topicos dominantes

--- sklearnLDA.py
+++ sklearnLDA.py
@@ -16,7 +16,7 @@ def display_topics(H, W, feature_names, documents, no_top_words, no_top_document
     #print("PREPARANDO ARCHIVO .CSV PARA VOLCAR PARAMETROS...")
     #archivo = open(nombreCSV, "a")
 
-    archivo = open('Resultados/txt/LDA-' + str(nTopics) + '-' + str(alpha) + '-' + str(eta) + '-SonyNeg.txt', 'w')   #PARA QUE SALGA EN FORMATO TXT
+    archivo = open('Resultados/txt/LDA-' + str(nTopics) + '-' + str(alpha) + '-' + str(eta) + '-MicroNeg.txt', 'w')   #PARA QUE SALGA EN FORMATO TXT
 
     for topic_idx, topic in enumerate(H):
         print("Topic %d:" % (topic_idx))
@@ -82,4 +82,7 @@ topicnames = ["Topic" + str(i) for i in range(lda_model.n_components)]
 docnames = df["Unnamed: 0"]
 # Make the pandas dataframe
 df_document_topic = pd.DataFrame(np.round(lda_W, 2), columns=topicnames, index=docnames)
+# dominant topic for each document
+dom_topic = np.argmax(df_document_topic.values, axis = 1)
+df_document_topic["Dom_Topic"] = dom_topic
 df_document_topic.to_csv("IntuitTMNeg.csv")
